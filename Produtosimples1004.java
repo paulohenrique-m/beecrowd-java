782bef2caa84557881c632e3edc2e73e2a6781da
paulohenrique-m <paulohenriquemenezes00@gmail.com>
2022-11-10 15:57:30 -0300

    first commit

---
+++ Produtosimples1004.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Produtosimples1004 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int PROD = a * b;
+        System.out.println("PROD = " + PROD);
+
+    }
+}
